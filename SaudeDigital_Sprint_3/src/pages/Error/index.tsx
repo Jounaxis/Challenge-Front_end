b1a8d8cb612a14103db9ef365556e68574bf8819
Lucas06-ux <lubgian@gmail.com>
2025-09-25 20:05:05 -0300

    refaturando a pagina de erro e estilizando a mesma para ficar mais alinhada ao estilo do projeto

--- SaudeDigital_Sprint_3/src/pages/Error/index.tsx
+++ SaudeDigital_Sprint_3/src/pages/Error/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useEffect } from "react";
 
 
@@ -8,10 +9,19 @@ const Error = () => {
     }, []);
 
     return (
-        <main>
-            <h1>
-                404
-            </h1>
+        <main className="container_pagina_erro">
+            <section className="container_pagina_erro_conteudo">
+                <div>
+                    <h1 className="pagina_erro_titulo">404</h1>
+                    <h2 className="pagina_erro_subtitulo">Página não encontrada</h2>
+                    <p className="pagina_erro_paragrafo">
+                        Ops! A página que você procura não existe ou foi removida.
+                    </p>
+                    <Link to="/" className="pagina_erro_botao_voltar">
+                        Voltar para a Home
+                    </Link>
+                </div>
+            </section>
         </main>
     );
 }
